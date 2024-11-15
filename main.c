c7ddcf29bd49b7b13ea2fedfd7b4ef42569ea68d
JIgorDev-sketch <joaoigorsf990@gmail.com>
2024-11-15 14:19:31 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,40 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
+	int **vetor, tamanho, i;
+	scanf("%d", &tamanho);
+	
+	if(tamanho == 0)
+	printf("[Vazio]\n");
+	
+	
+	
+	vetor = (int **)malloc(tamanho * sizeof(int *));
+	
+	for(i = 0; i < tamanho; i++)
+	scanf("%d", &vetor[i]);
+	
+	if(tamanho != 0){
+	
+	printf("[");
+	
+	for(i = 0; i < tamanho; i++)
+	if(i < tamanho - 1)
+	printf("%d,", vetor[i]);
 
-    //insira aqui seu código
-    printf("Nao implementado ainda...");
-
-    return 0;
+    else
+    printf("%d", vetor[i]);
+    
+	printf("]");
+	
+	printf("\n");
+    }
+	
+	for(i = 0; i < tamanho; i++)
+	free(vetor[i]);
+	
+	free(vetor);
+	
+return 0;
 }
