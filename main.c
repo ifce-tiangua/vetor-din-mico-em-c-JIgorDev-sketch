4e03e3745485b5c97f534423a03cc2aea8a438ef
JIgorDev-sketch <joaoigorsf990@gmail.com>
2024-11-18 09:41:07 -0300

    Update main.c

--- main.c
+++ main.c
@@ -6,24 +6,25 @@ int main(){
 	
 	scanf("%d", &tamanho);
 	
-	if(tamanho == 0){
+	vetor = malloc(tamanho * sizeof(int));
+	
+	if(tamanho <= 0){
 	   printf("[vazio]\n");
 	   return 0;
     }
 	
-	vetor = (int *)malloc(tamanho * sizeof(int));
-	
+	else{
 	for(i = 0; i < tamanho; i++)
 	scanf("%d", &vetor[i]);
 	
 	printf("[");
 	
 	for(i = 0; i < tamanho - 1; i++){
-	printf("%d,", vetor[i]);
+	printf("%d, ", vetor[i]);
     }
-    
     printf("%d]\n", vetor[i]);
-	
+   }
+   
 	free(vetor);
 	
 return 0;
